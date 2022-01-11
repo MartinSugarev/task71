0bbf1fb5657f344b25edf45bebd6cda38643af5a
Martin <mdsugarev@gmail.com>
2022-01-11 22:16:35 +0200

    r

--- src/Document.js
+++ src/Document.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './Document.css'
 
 export default function Document({title, content}){
@@ -25,7 +25,7 @@ export default function Document({title, content}){
     }
     return(
      <div>
-        <h1 className="title">{title}</h1>
+      <h1 className="title">{title}</h1>
       <p onScroll={handleScroll} ref={ref} className="content">{content}</p>
       <button disabled={able} >I Agree</button>
      </div>
